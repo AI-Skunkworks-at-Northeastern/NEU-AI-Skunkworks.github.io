8604175c0e8ba43e7a2945877854f7231176bb66
Jugal Joshi <jugaljoshi96@gmail.com>
2021-01-01 22:44:32 -0500

    Pushing changes to the master branch

---
+++ assets/js/slick-custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+  var slickOpts = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    adaptiveHeight: true,
+  };
+  $('.testimonials').slick(slickOpts);
+  var slickEnabled = true;
+});
